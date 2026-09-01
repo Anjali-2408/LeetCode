ad7f1f79deb2699216c330958e7ff4804ca6c5fa
Anjali Tiwari <anjalitiwari5517@gmail.com>
2026-09-01 21:46:28 +0530

    Time: 1207 ms (39.8%), Space: 414.9 MB (29.59%) - LeetHub

--- 3568-minimum-moves-to-clean-the-classroom/3568-minimum-moves-to-clean-the-classroom.cpp
+++ 3568-minimum-moves-to-clean-the-classroom/3568-minimum-moves-to-clean-the-classroom.cpp
@@ -3,101 +3,66 @@ public:
     int minMoves(vector<string>& classroom, int energy) {
         int m = classroom.size();
         int n = classroom[0].size();
-
-        int sr = -1, sc = -1;
-        int cnt = 0;
-
-        // Give each litter an ID for bitmask
-        vector<vector<int>> id(m, vector<int>(n, -1));
-
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
-                if(classroom[i][j] == 'S'){
-                    sr = i;
-                    sc = j;
-                }
-                if(classroom[i][j] == 'L'){
-                    id[i][j] = cnt++;
+        vector<vector<int>> litterMap(m, vector<int>(n, -1));
+        int startX = -1, startY = -1;
+        int litterCount = 0;
+        
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (classroom[i][j] == 'S') {
+                    startX = i;
+                    startY = j;
+                } else if (classroom[i][j] == 'L') {
+                    litterMap[i][j] = litterCount++;
                 }
             }
         }
-
-        int masks = 1 << cnt;
-        int fullMask = masks - 1;
-
-        // best[r][c][mask] = max energy reached at this state
-        vector<vector<vector<int>>> best
-        best.resize(m,vector<vector<int>>(n,vector<int>(masks,-1)));
-
-        struct State {
-            int r, c;
-            int mask;
-            int en;
-            int dist;
-        };
-
-        queue<State> q;
-        q.push({sr, sc, 0, energy, 0});
-        best[sr][sc][0] = energy;
-
-        int dr[] = {-1, 1, 0, 0};
-        int dc[] = {0, 0, -1, 1};
-
-        while(!q.empty()){
-            State cur = q.front();
+        
+        if (litterCount == 0) return 0;
+        
+        int targetMask = (1 << litterCount) - 1;
+        queue<tuple<int, int, int, int, int>> q;
+        q.push({startX, startY, energy, 0, 0});
+        
+        vector<vector<vector<vector<bool>>>> visited(m, vector<vector<vector<bool>>>(n, vector<vector<bool>>(energy + 1, vector<bool>(1 << litterCount, false))));
+        visited[startX][startY][energy][0] = true;
+        
+        int dirs[5] = {-1, 0, 1, 0, -1};
+        
+        while (!q.empty()) {
+            auto [r, c, e, mask, steps] = q.front();
             q.pop();
-
-            int r = cur.r;
-            int c = cur.c;
-            int mask = cur.mask;
-            int en = cur.en;
-            int dist = cur.dist;
-
-            // All litter collected
-            if(mask == fullMask){
-                return dist;
-            }
-            // No energy, cannot move
-            if(en == 0){
-                continue;
-            }
-
-            for(int d = 0; d < 4; d++){
-                int nr = r + dr[d];
-                int nc = c + dc[d];
-
-                // Outside grid
-                if(nr < 0 || nr >= m || nc < 0 || nc >= n){
-                    continue;
+            
+            for (int i = 0; i < 4; ++i) {
+                int nr = r + dirs[i];
+                int nc = c + dirs[i + 1];
+                
+                if (nr >= 0 && nr < m && nc >= 0 && nc < n && classroom[nr][nc] != 'X') {
+                    int nxt_e = e - 1;
+                    int nxt_mask = mask;
+                    
+                    if (classroom[nr][nc] == 'L') {
+                        nxt_mask |= (1 << litterMap[nr][nc]);
+                    }
+                    
+                    if (nxt_mask == targetMask) {
+                        return steps + 1;
+                    }
+                    
+                    if (classroom[nr][nc] == 'R') {
+                        nxt_e = energy;
+                    }
+                    
+                    if (nxt_e == 0 && classroom[nr][nc] != 'R') continue;
+                    
+                    if (!visited[nr][nc][nxt_e][nxt_mask]) {
+                        visited[nr][nc][nxt_e][nxt_mask] = true;
+                        q.push({nr, nc, nxt_e, nxt_mask, steps + 1});
+                    }
                 }
-
-                // Obstacle
-                if(classroom[nr][nc] == 'X'){
-                    continue;
-                }
-
-                int newEn = en - 1;
-                int newMask = mask;
-
-                // Collect litter
-                if(classroom[nr][nc] == 'L'){
-                    newMask |= (1 << id[nr][nc]);
-                }
-
-                // Recharge
-                if(classroom[nr][nc] == 'R'){
-                    newEn = energy;
-                }
-
-                // Already reached with more energy
-                if(best[nr][nc][newMask] >= newEn){
-                    continue;
-                }
-
-                best[nr][nc][newMask] = newEn;
-                q.push({nr, nc, newMask, newEn, dist + 1});
             }
         }
+        
         return -1;
     }
 };
